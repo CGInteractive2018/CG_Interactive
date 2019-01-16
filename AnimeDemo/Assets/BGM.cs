7690b63483e4e890dc11b9077df5048890a50ce7
HasegawaNobutake <16t2135d@shinshu-u.ac.jp>
2019-01-16 14:36:17 +0900

    BGMの追加

---
+++ AnimeDemo/Assets/BGM.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BGM : MonoBehaviour
+{
+    public AudioClip audioClip1;
+    private AudioSource audioSource;
+
+    void Start()
+    {
+        audioSource = gameObject.GetComponent<AudioSource>();
+        audioSource.clip = audioClip1;
+        audioSource.Play();
+    }
+
+}
