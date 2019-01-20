e78f057106bcfebe7102195e736273183cecfd7f
noda_325 <orange325ny@gmail.com>
2019-01-20 21:17:20 +0900

    rayを使ったSpGeneratorの追加

---
+++ AnimeDemo/Assets/Scripts/SpGeneratorRay.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpGeneratorRay : MonoBehaviour
+{
+
+    public GameObject Sphere;
+    public GameObject effect;
+    public float spn = 1.0f;
+    float dlt = 0;
+    GameObject target;
+    GameObject camera;
+    public float high = 1f;
+    public float bulletPower = 500f;
+    public static int remain;
+    //銃撃音
+    public AudioClip audioClip1;
+    private AudioSource audioSource;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        target = GameObject.FindWithTag("Player");
+        camera = GameObject.FindWithTag("MainCamera");
+        remain = 10000;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xpos = target.transform.position.x;
+        float ypos = target.transform.position.y;
+        float zpos = target.transform.position.z;
+
+        //カメラの現在位置取得
+        Vector3 cpos = camera.transform.position;
+        //z軸の設定
+        cpos.z = 0f;
+
+
+        //球を飛ばす方向ベクトルの取得
+        var heading = target.transform.position - camera.transform.position;
+        heading.y = (target.transform.position.y + 1) - camera.transform.position.y;
+
+        //方向ベクトルの正規化        
+        var distance = heading.magnitude;
+        var direction = heading / distance;
+
+        this.dlt += Time.deltaTime;
+        //if (Input.GetKeyDown("joystick button 7"))
+        if (remain != 0)
+        {
+            if (Input.GetButtonDown("ZR"))
+            {
+                //プレイヤーの向きの正規化
+                direction.Normalize();
+
+                Vector3 pos = new Vector3(xpos + direction.x, 1.1f, zpos + direction.z);
+                var point = ray.point - pos;
+                Debug.Log(point);
+
+                GameObject bullet = Instantiate(Sphere, pos, Quaternion.identity);
+                GameObject MazzuleFlashe = Instantiate(effect, pos, Quaternion.identity);
+                MazzuleFlashe.transform.rotation = target.transform.rotation;
+                MazzuleFlashe.transform.Rotate(new Vector3(0.0f, 90f, 0.0f));
+                Rigidbody rd = bullet.transform.GetComponent<Rigidbody>();
+                //rd.AddForce(direction.x * bulletPower, direction.y * bulletPower, direction.z * bulletPower);
+                rd.AddForce(point.x * bulletPower, point.y * bulletPower, point.z * bulletPower);
+
+                //銃撃音の再生
+                audioSource = gameObject.GetComponent<AudioSource>();
+                audioSource.clip = audioClip1;
+                audioSource.Play();
+
+                remain--;
+            }
+        }
+    }
+}
---
+++ AnimeDemo/Assets/Scripts/ray.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI; // ←※これを忘れずに入れる
+
+public class ray : MonoBehaviour
+{
+    public static Vector3 point;
+
+    void Start()
+    {
+        // スライダーを取得する
+
+    }
+
+    float _hp = 0;
+    void Update()
+    {
+        Vector3 center = new Vector3(Screen.width / 2, Screen.height / 2);
+        Ray ray = Camera.main.ScreenPointToRay(center);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, 1000.0f)) {
+           // Debug.Log(hit.point);
+            point = hit.point;
+        }
+        else
+        {
+
+        }
+    }
+}
